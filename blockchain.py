07f4275a6c4305ba5d1e365580af6f14823dc16f
codingzhe <986523239@qq.com>
2020-02-12 21:38:26 +0800

    finish the function of adding new block

--- blockchain.py
+++ blockchain.py
@@ -11,14 +11,32 @@
 #     'proof': '',
 #     'previous_hash': '',
 # }
+from time import time
+import hashlib
+import json
 '''区块类结构'''
 class Blockchain:
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        # 创世纪区块
+        self.new_block(proof=100, previous_hash=1)
+
     # 新添加一个快
-    def new_block(self):
-        pass
+    def new_block(self, proof, previous_hash = None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.last_block)
+        }
+        # 打包之后将交易清空
+        self.current_transactions = []
+        self.chain.append(block)
+
+        return block
+
 
     # add new transcation
     def new_transaction(self, sender, recipient, amount) -> int:
@@ -33,9 +51,12 @@ class Blockchain:
     # hash static function
     @staticmethod
     def hash(block):
-        pass
+        # 将block转化成一个字节数组
+        block_string = json.dump(block, sort_keys=True).encode()
+        # 返回哈希之后的摘要信息
+        return hashlib.sha256(block_string).hexdigest()
 
     # get last block in blockchain
     @property
     def last_block(self):
-        pass
+        return self.chain[-1]
