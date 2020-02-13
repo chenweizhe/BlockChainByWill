04efa0360fe7ec8f78ecb37a09f9f2a8c9bd1c52
codingzhe <986523239@qq.com>
2020-02-13 16:41:03 +0800

    finish the consensus

--- blockchain.py
+++ blockchain.py
@@ -18,13 +18,68 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from uuid import uuid4
+from urllib.parse import urlparse
+import requests
+from argparse import ArgumentParser
 '''区块类结构'''
 class Blockchain:
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        # 保存节点信息  set集合中的元素是独一无二的 不可重复的 数组的元素是可以重复的
+        self.nodes = set()
+
         # 创世纪区块
         self.new_block(proof=100, previous_hash=1)
+
+
+    #  注册一个节点
+    def register_node(self, address: str):
+        # 地址形式一般是http://127.0.0.1:5001
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    # 检查链表是否合法
+    def valid_chain(self, chain) -> bool:
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+
+            # 不合法的第一种情况：块的哈希不等于计算出来的哈希 则这是一条虚假的链
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # 不合法的第二种情况：工作量证明不合格
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+            last_block = block
+            current_index += 1
+
+        return True
+
+    # 解决冲突 选择最长链
+    def resolve_conflicts(self) -> bool:
+        neighbours = self.nodes
+        max_length = len(self.chain)
+        new_chain = None
+
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length= length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
 
     # 新添加一个快
     def new_block(self, proof, previous_hash = None):
@@ -88,8 +143,12 @@ class Blockchain:
         #     return  False
         # print(guess_hash)
         return guess_hash[0:4] == '0000'
-# 在现实生活中 比特币是以十八个0开头的 这个只是演示
+        # 在现实生活中 比特币是以十八个0开头的 这个只是演示
 
+
+
+
+'''======================================================================='''
 # 通过flask实现与其他节点的通信
 # flask是轻量级的web server
 app = Flask(__name__)
@@ -149,7 +208,45 @@ def full_chain():
     # 将json转化成字符串
     return jsonify(reponse), 200
 
+# 接受来自一个节点的请求 注册节点
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return 'Error! please supply a valid list of nodes', 400
+    for node in nodes:
+        blockchain.register_node(node)
+    response = {
+        'message': 'new nodes have been added',
+        'total_nodes': list(blockchain.nodes)
+    }
+    return jsonify(response), 201
+
+# 解决冲突的请求
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain was authoritative',
+            'chain': blockchain.chain
+        }
+    return jsonify(response), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+    parser = ArgumentParser()
+    parser.add_argument('-p','--port',default=5000, type=int, help='port to listen to')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0', port=port)
 
